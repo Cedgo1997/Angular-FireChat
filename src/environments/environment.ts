830e8e0ced74165eb1cac71a4f9c328c2d5eeb82
César Gonzalez <cedgo1997@gmail.com>
2020-08-01 18:55:45 -0400

    angularfire configured!

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5c2Lp3RJDteWygU9WPl7VV4TibMpUHc0",
+    authDomain: "firechat-a2bac.firebaseapp.com",
+    databaseURL: "https://firechat-a2bac.firebaseio.com",
+    projectId: "firechat-a2bac",
+    storageBucket: "firechat-a2bac.appspot.com",
+    messagingSenderId: "668366866416",
+    appId: "1:668366866416:web:560463b49c0f67af58bf35",
+    measurementId: "G-B68BEX3TR6"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
