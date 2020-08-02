b350786fe8fb06c70519bf1138679e722fd265b9
César Gonzalez <cedgo1997@gmail.com>
2020-08-01 22:38:50 -0400

    agregar mensaje method added

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ChatService } from 'src/app/providers/chat.service';
+import { Component, OnInit } from "@angular/core";
+import { ChatService } from "src/app/providers/chat.service";
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styles: []
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styles: [],
 })
 export class ChatComponent {
-
   mensaje: string = "";
 
-  constructor( public _cs:ChatService ) { 
-
+  constructor(public _cs: ChatService) {
     this._cs.cargarMensajes().subscribe();
-
   }
 
   enviarMensaje() {
     console.log(this.mensaje);
-  }
 
+    if (this.mensaje.length === 0) {
+      return;
+    }
+    this._cs
+      .agregarMensaje(this.mensaje)
+      .then(() => this.mensaje = '')
+      .catch((err) => console.error("Error al enviar", err));
+  }
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -3,24 +3,38 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from "@angular/fire/firestore";
-import { Observable } from "rxjs";
-import { Mensaje } from './../interfaces/mensaje.interface';
+import { map } from "rxjs/operators";
+import { Mensaje } from "./../interfaces/mensaje.interface";
 
 @Injectable({
   providedIn: "root",
 })
-
-
 export class ChatService {
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
-  items: Observable<Mensaje[]>;
-
   public chats: Mensaje[] = [];
 
   constructor(private afs: AngularFirestore) {}
 
   cargarMensajes() {
     this.itemsCollection = this.afs.collection<Mensaje>("chats");
-    return this.itemsCollection.valueChanges();
+
+    return this.itemsCollection.valueChanges().pipe(
+      map((mensajes: Mensaje[]) => {
+        console.log(mensajes);
+        this.chats = mensajes;
+      })
+    );
+  }
+
+  // TODO falta UUID del usuario
+
+  agregarMensaje(texto: string) {
+    let mensaje: Mensaje = {
+      nombre: "César Demo",
+      mensaje: texto,
+      fecha: new Date().getTime(),
+    };
+
+    return this.itemsCollection.add( mensaje );
   }
 }
