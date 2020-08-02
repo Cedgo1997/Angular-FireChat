e8125076610953a16372828d8b0a3a467ea92f50
César Gonzalez <cedgo1997@gmail.com>
2020-08-01 21:49:26 -0400

    import mensaje interface and using as type

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -4,20 +4,23 @@ import {
   AngularFirestoreCollection,
 } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
+import { Mensaje } from './../interfaces/mensaje.interface';
 
 @Injectable({
   providedIn: "root",
 })
-export class ChatService {
-  private itemsCollection: AngularFirestoreCollection<any>;
-  items: Observable<any[]>;
 
-  public chats: any[] = [];
+
+export class ChatService {
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
+  items: Observable<Mensaje[]>;
+
+  public chats: Mensaje[] = [];
 
   constructor(private afs: AngularFirestore) {}
 
   cargarMensajes() {
-    this.itemsCollection = this.afs.collection<any>("chats");
+    this.itemsCollection = this.afs.collection<Mensaje>("chats");
     return this.itemsCollection.valueChanges();
   }
 }
